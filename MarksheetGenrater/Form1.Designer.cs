ae27e179dbd7572521e9fc5cbdb9ba2ca4b6b57a
AbhishekChoksi <a.p.choksi420@gmail.com>
2021-02-20 15:35:55 +0530

    Add project files.

---
+++ MarksheetGenrater/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace MarksheetGenrater
+{
+    partial class homepage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.lbl_enro = new System.Windows.Forms.Label();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.txt_enrolment = new System.Windows.Forms.TextBox();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.lbl_gui = new System.Windows.Forms.Label();
+            this.lbl_java = new System.Windows.Forms.Label();
+            this.lbl_oose = new System.Windows.Forms.Label();
+            this.lbl_linux = new System.Windows.Forms.Label();
+            this.lbl_cs = new System.Windows.Forms.Label();
+            this.txt_gui = new System.Windows.Forms.TextBox();
+            this.txt_java = new System.Windows.Forms.TextBox();
+            this.txt_linux = new System.Windows.Forms.TextBox();
+            this.txt_cs = new System.Windows.Forms.TextBox();
+            this.txt_oose = new System.Windows.Forms.TextBox();
+            this.lbl_total = new System.Windows.Forms.Label();
+            this.lbl_totalshow = new System.Windows.Forms.Label();
+            this.lbl_pershow = new System.Windows.Forms.Label();
+            this.lbl_per = new System.Windows.Forms.Label();
+            this.lbl_grade = new System.Windows.Forms.Label();
+            this.lbl_gradeshow = new System.Windows.Forms.Label();
+            this.bttn_calculate = new System.Windows.Forms.Button();
+            this.bttn_reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Font = new System.Drawing.Font("Courier New", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.ForeColor = System.Drawing.Color.Crimson;
+            this.lbl_title.Location = new System.Drawing.Point(174, 9);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(350, 31);
+            this.lbl_title.TabIndex = 0;
+            this.lbl_title.Text = "Marksheet Calculation";
+            // 
+            // lbl_enro
+            // 
+            this.lbl_enro.AutoSize = true;
+            this.lbl_enro.Location = new System.Drawing.Point(13, 59);
+            this.lbl_enro.Name = "lbl_enro";
+            this.lbl_enro.Size = new System.Drawing.Size(132, 24);
+            this.lbl_enro.TabIndex = 1;
+            this.lbl_enro.Text = "Enrolment No.";
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(13, 124);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(61, 24);
+            this.lbl_name.TabIndex = 2;
+            this.lbl_name.Text = "Name";
+            // 
+            // txt_enrolment
+            // 
+            this.txt_enrolment.Location = new System.Drawing.Point(152, 59);
+            this.txt_enrolment.Name = "txt_enrolment";
+            this.txt_enrolment.Size = new System.Drawing.Size(229, 29);
+            this.txt_enrolment.TabIndex = 3;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(152, 124);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(229, 29);
+            this.txt_name.TabIndex = 4;
+            // 
+            // lbl_gui
+            // 
+            this.lbl_gui.AutoSize = true;
+            this.lbl_gui.Location = new System.Drawing.Point(13, 195);
+            this.lbl_gui.Name = "lbl_gui";
+            this.lbl_gui.Size = new System.Drawing.Size(41, 24);
+            this.lbl_gui.TabIndex = 5;
+            this.lbl_gui.Text = "GUI";
+            // 
+            // lbl_java
+            // 
+            this.lbl_java.AutoSize = true;
+            this.lbl_java.Location = new System.Drawing.Point(13, 245);
+            this.lbl_java.Name = "lbl_java";
+            this.lbl_java.Size = new System.Drawing.Size(48, 24);
+            this.lbl_java.TabIndex = 6;
+            this.lbl_java.Text = "Java";
+            // 
+            // lbl_oose
+            // 
+            this.lbl_oose.AutoSize = true;
+            this.lbl_oose.Location = new System.Drawing.Point(316, 195);
+            this.lbl_oose.Name = "lbl_oose";
+            this.lbl_oose.Size = new System.Drawing.Size(65, 24);
+            this.lbl_oose.TabIndex = 7;
+            this.lbl_oose.Text = "OOSE";
+            // 
+            // lbl_linux
+            // 
+            this.lbl_linux.AutoSize = true;
+            this.lbl_linux.Location = new System.Drawing.Point(13, 295);
+            this.lbl_linux.Name = "lbl_linux";
+            this.lbl_linux.Size = new System.Drawing.Size(56, 24);
+            this.lbl_linux.TabIndex = 8;
+            this.lbl_linux.Text = "Linux";
+            // 
+            // lbl_cs
+            // 
+            this.lbl_cs.AutoSize = true;
+            this.lbl_cs.Location = new System.Drawing.Point(316, 242);
+            this.lbl_cs.Name = "lbl_cs";
+            this.lbl_cs.Size = new System.Drawing.Size(132, 24);
+            this.lbl_cs.TabIndex = 9;
+            this.lbl_cs.Text = "Cyber Security";
+            // 
+            // txt_gui
+            // 
+            this.txt_gui.Location = new System.Drawing.Point(104, 195);
+            this.txt_gui.Name = "txt_gui";
+            this.txt_gui.Size = new System.Drawing.Size(174, 29);
+            this.txt_gui.TabIndex = 10;
+            // 
+            // txt_java
+            // 
+            this.txt_java.Location = new System.Drawing.Point(104, 245);
+            this.txt_java.Name = "txt_java";
+            this.txt_java.Size = new System.Drawing.Size(174, 29);
+            this.txt_java.TabIndex = 11;
+            // 
+            // txt_linux
+            // 
+            this.txt_linux.Location = new System.Drawing.Point(104, 295);
+            this.txt_linux.Name = "txt_linux";
+            this.txt_linux.Size = new System.Drawing.Size(174, 29);
+            this.txt_linux.TabIndex = 12;
+            // 
+            // txt_cs
+            // 
+            this.txt_cs.Location = new System.Drawing.Point(454, 242);
+            this.txt_cs.Name = "txt_cs";
+            this.txt_cs.Size = new System.Drawing.Size(174, 29);
+            this.txt_cs.TabIndex = 14;
+            // 
+            // txt_oose
+            // 
+            this.txt_oose.Location = new System.Drawing.Point(454, 195);
+            this.txt_oose.Name = "txt_oose";
+            this.txt_oose.Size = new System.Drawing.Size(174, 29);
+            this.txt_oose.TabIndex = 13;
+            // 
+            // lbl_total
+            // 
+            this.lbl_total.AutoSize = true;
+            this.lbl_total.Location = new System.Drawing.Point(316, 295);
+            this.lbl_total.Name = "lbl_total";
+            this.lbl_total.Size = new System.Drawing.Size(51, 24);
+            this.lbl_total.TabIndex = 15;
+            this.lbl_total.Text = "Total";
+            // 
+            // lbl_totalshow
+            // 
+            this.lbl_totalshow.AutoSize = true;
+            this.lbl_totalshow.Location = new System.Drawing.Point(454, 295);
+            this.lbl_totalshow.Name = "lbl_totalshow";
+            this.lbl_totalshow.Size = new System.Drawing.Size(180, 24);
+            this.lbl_totalshow.TabIndex = 17;
+            this.lbl_totalshow.Text = "                                  ";
+            this.lbl_totalshow.Visible = false;
+            // 
+            // lbl_pershow
+            // 
+            this.lbl_pershow.AutoSize = true;
+            this.lbl_pershow.Location = new System.Drawing.Point(454, 354);
+            this.lbl_pershow.Name = "lbl_pershow";
+            this.lbl_pershow.Size = new System.Drawing.Size(180, 24);
+            this.lbl_pershow.TabIndex = 19;
+            this.lbl_pershow.Text = "                                  ";
+            this.lbl_pershow.Visible = false;
+            // 
+            // lbl_per
+            // 
+            this.lbl_per.AutoSize = true;
+            this.lbl_per.Location = new System.Drawing.Point(316, 354);
+            this.lbl_per.Name = "lbl_per";
+            this.lbl_per.Size = new System.Drawing.Size(107, 24);
+            this.lbl_per.TabIndex = 18;
+            this.lbl_per.Text = "Percentage";
+            // 
+            // lbl_grade
+            // 
+            this.lbl_grade.AutoSize = true;
+            this.lbl_grade.Location = new System.Drawing.Point(13, 354);
+            this.lbl_grade.Name = "lbl_grade";
+            this.lbl_grade.Size = new System.Drawing.Size(62, 24);
+            this.lbl_grade.TabIndex = 20;
+            this.lbl_grade.Text = "Grade";
+            // 
+            // lbl_gradeshow
+            // 
+            this.lbl_gradeshow.AutoSize = true;
+            this.lbl_gradeshow.Location = new System.Drawing.Point(104, 354);
+            this.lbl_gradeshow.Name = "lbl_gradeshow";
+            this.lbl_gradeshow.Size = new System.Drawing.Size(180, 24);
+            this.lbl_gradeshow.TabIndex = 21;
+            this.lbl_gradeshow.Text = "                                  ";
+            this.lbl_gradeshow.Visible = false;
+            // 
+            // bttn_calculate
+            // 
+            this.bttn_calculate.Location = new System.Drawing.Point(429, 96);
+            this.bttn_calculate.Name = "bttn_calculate";
+            this.bttn_calculate.Size = new System.Drawing.Size(95, 29);
+            this.bttn_calculate.TabIndex = 22;
+            this.bttn_calculate.Text = "Calculate";
+            this.bttn_calculate.UseVisualStyleBackColor = true;
+            this.bttn_calculate.Click += new System.EventHandler(this.bttn_calculate_Click);
+            // 
+            // bttn_reset
+            // 
+            this.bttn_reset.Location = new System.Drawing.Point(557, 96);
+            this.bttn_reset.Name = "bttn_reset";
+            this.bttn_reset.Size = new System.Drawing.Size(95, 29);
+            this.bttn_reset.TabIndex = 23;
+            this.bttn_reset.Text = "Reset";
+            this.bttn_reset.UseVisualStyleBackColor = true;
+            this.bttn_reset.Click += new System.EventHandler(this.bttn_reset_Click);
+            // 
+            // homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 393);
+            this.Controls.Add(this.bttn_reset);
+            this.Controls.Add(this.bttn_calculate);
+            this.Controls.Add(this.lbl_gradeshow);
+            this.Controls.Add(this.lbl_grade);
+            this.Controls.Add(this.lbl_pershow);
+            this.Controls.Add(this.lbl_per);
+            this.Controls.Add(this.lbl_totalshow);
+            this.Controls.Add(this.lbl_total);
+            this.Controls.Add(this.txt_cs);
+            this.Controls.Add(this.txt_oose);
+            this.Controls.Add(this.txt_linux);
+            this.Controls.Add(this.txt_java);
+            this.Controls.Add(this.txt_gui);
+            this.Controls.Add(this.lbl_linux);
+            this.Controls.Add(this.lbl_java);
+            this.Controls.Add(this.lbl_gui);
+            this.Controls.Add(this.lbl_cs);
+            this.Controls.Add(this.lbl_oose);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.txt_enrolment);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.lbl_enro);
+            this.Controls.Add(this.lbl_title);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "homepage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Marksheet Calculation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Label lbl_enro;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.TextBox txt_enrolment;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label lbl_gui;
+        private System.Windows.Forms.Label lbl_java;
+        private System.Windows.Forms.Label lbl_oose;
+        private System.Windows.Forms.Label lbl_linux;
+        private System.Windows.Forms.Label lbl_cs;
+        private System.Windows.Forms.TextBox txt_gui;
+        private System.Windows.Forms.TextBox txt_java;
+        private System.Windows.Forms.TextBox txt_linux;
+        private System.Windows.Forms.TextBox txt_cs;
+        private System.Windows.Forms.TextBox txt_oose;
+        private System.Windows.Forms.Label lbl_total;
+        private System.Windows.Forms.Label lbl_totalshow;
+        private System.Windows.Forms.Label lbl_pershow;
+        private System.Windows.Forms.Label lbl_per;
+        private System.Windows.Forms.Label lbl_grade;
+        private System.Windows.Forms.Label lbl_gradeshow;
+        private System.Windows.Forms.Button bttn_calculate;
+        private System.Windows.Forms.Button bttn_reset;
+    }
+}
+
---
+++ MarksheetGenrater/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarksheetGenrater
+{
+    public partial class homepage : Form
+    {
+        public homepage()
+        {
+            InitializeComponent();
+        }
+
+        private void bttn_calculate_Click(object sender, EventArgs e)
+        {
+            int m1, m2, m3, m4, m5, total = 0;
+            double percentage;
+            char grade = '\0';
+
+            m1 = Convert.ToInt32(txt_gui.Text);
+            m2 = Convert.ToInt32(txt_java.Text);
+            m3 = Convert.ToInt32(txt_linux.Text);
+            m4 = Convert.ToInt32(txt_oose.Text);
+            m5 = Convert.ToInt32(txt_cs.Text);
+
+            total = m1 + m2 + m3 + m4 + m5;
+
+            percentage = total * 100f / 500;
+
+            if (m1 < 35 || m2 < 35 || m3 < 35 || m4 < 35 || m5 < 35)
+            {
+                grade = 'F';
+            }
+            else if (percentage > 90)
+            {
+                grade = 'A';
+            }
+            else if (percentage <= 90 && percentage > 75)
+            {
+                grade = 'B';
+            }
+            else if (percentage <= 75 && percentage > 50)
+            {
+                grade = 'C';
+            }
+            else if (percentage <= 50 && percentage >= 35)
+            {
+                grade = 'D';
+            }
+
+            lbl_totalshow.Text = total.ToString();
+            lbl_pershow.Text = percentage.ToString() + " %";
+            lbl_gradeshow.Text = grade.ToString();
+
+            lbl_totalshow.Visible = lbl_pershow.Visible = lbl_gradeshow.Visible = true;
+        }
+
+        private void bttn_reset_Click(object sender, EventArgs e)
+        {
+            txt_enrolment.Text = "";
+            txt_name.Text = "";
+            txt_gui.Text = "";
+            txt_java.Text = "";
+            txt_linux.Text = "";
+            txt_oose.Text = "";
+            txt_cs.Text = "";
+            lbl_totalshow.Text = "";
+            lbl_pershow.Text = "";
+            lbl_gradeshow.Text = "";
+
+            lbl_totalshow.Visible = lbl_pershow.Visible = lbl_gradeshow.Visible = false;
+        }
+    }
+}
